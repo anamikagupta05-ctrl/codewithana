36088ff14afde05b48731c302e9f7cc37303b031
anamikagupta05-ctrl <anamika.gupta05@gmail.com>
2026-04-27 18:00:26 +0530

    Delete DjangoML/MLdjango directory

--- DjangoML/MLdjango/ml_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import PredictionModel
-
-class UploadFileForm(forms.ModelForm):
-    class Meta:
-            model = PredictionModel
-            fields = ['id','title', 'file']
